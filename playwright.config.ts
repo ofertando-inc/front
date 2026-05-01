a809ad566ecf4112cd28eadbb2225da92f263917
Booker <nicolaspatino77@gmail.com>
2026-05-01 18:41:29 +0200

    :rotating_light: add auth smoke e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-	webServer: { command: 'npm run build && npm run preview', port: 4173 },
+	use: {
+		baseURL: 'http://127.0.0.1:4173'
+	},
+	webServer: {
+		command: 'npm run build && npm run preview -- --host 127.0.0.1',
+		port: 4173
+	},
 	testMatch: '**/*.e2e.{ts,js}'
 });
